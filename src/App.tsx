36c9f9f04fbf1834d29acb1d757b259f231077fe
MatheusW166 <matheuswagnerdossantos@outlook.com>
2022-12-29 15:03:46 -0400

    chore: sass and sass-glob-import

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import { HelloWorld } from './components/HelloWorld';
 
 function App() {
 	return (
-		<p>Hello World</p>
+		<HelloWorld />
 	);
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import autoprefixer from 'autoprefixer';
+import sassGlobImports from 'vite-plugin-sass-glob-import';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), sassGlobImports()],
 	css: {
 		postcss: {
 			plugins: [
