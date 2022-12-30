0abdcac66974a4ab1e37c094c611036e9184f22a
MatheusW166 <matheuswagnerdossantos@outlook.com>
2022-12-30 16:06:32 -0400

    App.tsx: using pokemon object on PokemonCard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { PokemonCard } from "./components/PokemonCard/PokemonCard";
+import { Categories } from "./models/Categories";
 
 function App() {
 	return (
 		<div id="app" data-theme="light">
-			<PokemonCard pokemon={{ name: "Bulbassaur", order: 1, categories: [], image: "" }} />
+			<PokemonCard pokemon={{ name: "Bulbassaur", order: 1, categories: [Categories.Grass, Categories.Poison], image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" }} />
 		</div>
 	);
 }
