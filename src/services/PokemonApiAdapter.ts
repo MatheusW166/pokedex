8ab3716dc34d326df5d63c7dd5771f4731b80ca4
MatheusW166 <matheuswagnerdossantos@outlook.com>
2022-12-31 00:06:04 -0400

    chore: axios

--- src/services/PokemonApiAdapter.ts
+++ src/services/PokemonApiAdapter.ts
@@ -1,10 +1,20 @@
 import { PokemonRootEndpointResponseInterface } from "./endpoints_response_interfaces/pokemon_endpoint/PokemonRootEndpointResponseInterface";
 import { PokemonApiRequestProtocols } from "./PokemonApiRequestProtocols";
+import axios from "axios";
+
+const endpointPokemon = axios.create({
+  baseURL: "https://pokeapi.co/api/v2/pokemon/",
+  headers: { "Content-Type": "application/json" }
+});
 
 export class PokemonApiAdapter implements PokemonApiRequestProtocols {
   getPokemons(limit: number, offset: number): PokemonRootEndpointResponseInterface {
-    console.log(limit);
-    console.log(offset);
+    endpointPokemon.get("/", {
+      params: {
+        limit: limit ?? 30,
+        offset: offset ?? 0,
+      },
+    });
     return {
       count: 10,
       next: "",
