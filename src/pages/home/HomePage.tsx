1c43657fcc341c551eb20a7cd95ceff1f8304c64
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-01-02 16:22:08 -0400

    scss: responsivity added to home page

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,72 +1,15 @@
-import { useEffect, useState } from "react";
-import { PokemonApiAdapter } from "../../services/PokemonApiAdapter";
-import { PokemonRootEndpointResponse } from "../../services/endpoints/endpoint_pokemon/models/PokemonRootEndpointResponse";
-import { PokemonEndpointResponse } from "../../services/endpoints/endpoint_pokemon/models/PokemonEndpointResponse";
-import { isRequestError, RequestError } from "../../errors/RequestError";
-import { PokemonCard } from "../../components/PokemonCard/PokemonCard";
-import { Categories, toCategory } from "../../models/Categories";
-
-const client = new PokemonApiAdapter();
-type Pokemon = PokemonEndpointResponse;
-type DetailsPromise = Promise<Pokemon | RequestError>;
+import { useState } from "react";
+import { Pokedex } from "../../components/Pokedex/Pokedex";
 
 export function HomePage() {
-  const [pokemonPage, setPokemonPage] = useState<PokemonRootEndpointResponse>();
-  const [pokemonsPageDetails, setPokemonsPageDetails] = useState<Pokemon[]>();
-
-  async function getPokemonPage() {
-    const res = await client.getPokemons(20);
-    if (isRequestError(res)) {
-      return;
-    }
-    res.results;
-    setPokemonPage(res);
-  }
-
-  async function getPokemonsPageDetails() {
-    if (!pokemonPage) return;
-    const promises: DetailsPromise[] = [];
-    pokemonPage.results.forEach((item) => {
-      promises.push(client.getPokemonByName(item.name));
-    });
-
-    const promisesResult = await Promise.all(promises);
-    const resolved = promisesResult.filter((res) => !isRequestError(res));
-    setPokemonsPageDetails(resolved as Pokemon[]);
-  }
-
-  useEffect(() => {
-    getPokemonPage();
-  }, []);
-
-  useEffect(() => {
-    getPokemonsPageDetails();
-  }, [pokemonPage]);
-
-  function getPokemonImg(pokemon: Pokemon): string {
-    return (
-      pokemon.sprites.other.dream_world.front_default ??
-      pokemon.sprites.other["official-artwork"].front_default
-    );
-  }
-
-  function getPokemonCategories(pokemon: Pokemon): Categories[] {
-    return pokemon.types.map((type) => toCategory(type.type.name));
-  }
+  const [pokedexPage, setPokedexPage] = useState(0);
 
   return (
-    <div className="pokemon-page">
-      {pokemonsPageDetails?.map((pk, idx) => {
-        return (
-          <PokemonCard
-            key={idx}
-            order={pk.order}
-            name={pk.name}
-            image={getPokemonImg(pk)}
-            categories={getPokemonCategories(pk)}
-          />
-        );
-      })}
-    </div>
+    <section className="pokedex">
+      <h2>Pokedex</h2>
+      <div className="pokedex-list">
+        <Pokedex limit={20} offset={pokedexPage * 20} />
+      </div>
+    </section>
   );
 }
