74322614ff12ff36b7ed5c647b7e4d37895db884
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-01-02 01:49:24 -0400

    layout: some layout adjusts

--- src/components/PokemonCard/PokemonCard.tsx
+++ src/components/PokemonCard/PokemonCard.tsx
@@ -6,57 +6,65 @@ import { clsx } from "clsx";
 import "../../helpers/number.extensions";
 
 interface PokemonCardProps {
-	name: string;
-	image: string;
-	categories: Categories[];
-	order: number;
+  name: string;
+  image: string;
+  categories: Categories[];
+  order: number;
 }
 
 interface PokemonCardImgProps {
-	img: string;
-	hasShadow?: boolean;
-	className?: string;
+  img: string;
+  hasShadow?: boolean;
+  className?: string;
 }
 
 interface PokemonCardCategoriesBreadCupsProps {
-	categories: Categories[];
+  categories: Categories[];
 }
 
-function PokemonCardImg({ img, className, hasShadow = true }: PokemonCardImgProps) {
-	return (
-		<div className={clsx("pokemon-img", hasShadow && "shadow", className)}>
-			<img src={img}></img>
-		</div>
-	);
+function PokemonCardImg({
+  img,
+  className,
+  hasShadow = true
+}: PokemonCardImgProps) {
+  return (
+    <div className={clsx("pokemon-img", hasShadow && "shadow", className)}>
+      <img src={img}></img>
+    </div>
+  );
 }
 
-function PokemonCardCategoriesBreadCups({ categories }: PokemonCardCategoriesBreadCupsProps) {
-	return (
-		<>
-			{categories.map((name, idx) => <TypeBreadCup key={idx} type={name} />)}
-		</>
-	);
+function PokemonCardCategoriesBreadCups({
+  categories
+}: PokemonCardCategoriesBreadCupsProps) {
+  return (
+    <>
+      {categories.map((name, idx) => (
+        <TypeBreadCup key={idx} type={name} />
+      ))}
+    </>
+  );
 }
 
 function PokemonCard({ image, categories, name, order }: PokemonCardProps) {
-	return (
-		<article className="pokemon-card">
-			<PokemonCardImg img={image} />
-			<PokemonCardInnerBg category={categories[0]} />
-			<div className="types">
-				<div className="type-icon">
-					<PokemonTypeIcon category={categories[0]} />
-				</div>
-				<div className="type-names">
-					<PokemonCardCategoriesBreadCups categories={categories} />
-				</div>
-			</div>
-			<div className="characteristics">
-				<p>Nº {order.padStart(3, "0")}</p>
-				<h2>{name}</h2>
-			</div>
-		</article>
-	);
+  return (
+    <article className="pokemon-card">
+      <PokemonCardImg img={image} />
+      <PokemonCardInnerBg category={categories[0]} />
+      <div className="types">
+        <div className="type-icon">
+          <PokemonTypeIcon category={categories[0]} />
+        </div>
+        <div className="type-names">
+          <PokemonCardCategoriesBreadCups categories={categories} />
+        </div>
+      </div>
+      <div className="characteristics">
+        <p>Nº {order.padStart(3, "0")}</p>
+        <h2>{name.initCap()}</h2>
+      </div>
+    </article>
+  );
 }
 
 export { PokemonCard, PokemonCardCategoriesBreadCups, PokemonCardImg };
