f14b04e8a9c961699ec052aeee03264ae89a1b28
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-01-12 19:54:26 -0300

    ci: workflow script replaced

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,15 @@
 /* eslint-disable quotes */
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import autoprefixer from 'autoprefixer';
-import sassGlobImports from 'vite-plugin-sass-glob-import';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import autoprefixer from "autoprefixer";
+import sassGlobImports from "vite-plugin-sass-glob-import";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), sassGlobImports()],
-	css: {
-		postcss: {
-			plugins: [
-				autoprefixer({})
-			]
-		}
-	},
+  plugins: [react(), sassGlobImports()],
+  css: {
+    postcss: {
+      plugins: [autoprefixer({})]
+    }
+  }
 });
