2e1fc6ab7e7be6315ff159c5b02f46dfaeeaa285
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-01-12 20:10:42 -0300

    fix: routes changed, trying to fix blank screen

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,8 +4,8 @@ import { HomeView } from "./views/home/HomeView";
 export function MyRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/home" />} />
-      <Route path="/home" element={<HomeView />} />
+      <Route path="/pokedex" element={<Navigate to="/home" />} />
+      <Route path="/pokedex/home" element={<HomeView />} />
     </Routes>
   );
 }
