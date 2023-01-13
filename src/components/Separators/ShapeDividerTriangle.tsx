79f675cbc005260d389ac544b14d150afb31d5ad
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-01-13 20:32:04 -0300

    fix: some props added to svg in order to fix sizing problem

--- src/components/Separators/ShapeDividerTriangle.tsx
+++ src/components/Separators/ShapeDividerTriangle.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 
 export function ShapeDividerTriangle(props: React.SVGProps<SVGSVGElement>) {
-  return <svg
-    width={1440}
-    height={120}
-    fill="#fff"
-    xmlns="http://www.w3.org/2000/svg"
-    {...props}
-  >
-    <path d="M1440 120H0L1070.7 5.28 1440 120Z" fill="#F5F5F5" />
-  </svg>;
+  return (
+    <svg
+      width="1440"
+      viewBox="0 0 1440 120"
+      preserveAspectRatio="xMidYMax meet"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="shape-divider-triangle"
+      {...props}
+    >
+      <path d="M1440 120H0L1070.7 5.28 1440 120Z" />
+    </svg>
+  );
 }
