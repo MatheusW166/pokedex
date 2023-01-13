7ba1a8b7d6519bccdbb282b4f497c2aaeeac9349
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-01-13 20:33:09 -0300

    home view top style

--- src/views/home/HomeView.tsx
+++ src/views/home/HomeView.tsx
@@ -1,11 +1,25 @@
 import { Pokedex } from "../../components/Pokedex";
+import { LogoName, PokeballConcept } from "../../components/Logos";
+import { ShapeDividerTriangle } from "../../components/Separators";
 
 export function HomeView() {
   return (
     <div className="home">
       <main>
-        <section className="top"></section>
-        <section className="pokedex-page">
+        <section>
+          <div className="container">
+            <LogoName />
+            <div className="title">
+              <h1>Find your favorite Pokemons!</h1>
+              <p>Search and find all information about it.</p>
+            </div>
+            <PokeballConcept />
+          </div>
+          <div className="divider">
+            <ShapeDividerTriangle />
+          </div>
+        </section>
+        <section>
           <h2>Pokedex</h2>
           <Pokedex />
         </section>
