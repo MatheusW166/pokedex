c74ced167d24abfa2ba9d858691bb14d407bc858
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-01-16 15:44:31 -0300

    layout: mobile layout created

--- src/views/home/HomeView.tsx
+++ src/views/home/HomeView.tsx
@@ -1,6 +1,5 @@
 import { Pokedex } from "../../components/Pokedex";
 import { LogoName, PokeballConcept, Pokeball } from "../../components/Logos";
-import { ShapeDividerTriangle } from "../../components/Separators";
 
 export function HomeView() {
   return (
@@ -13,15 +12,12 @@ export function HomeView() {
               <h1>Find your favorite Pokemons!</h1>
               <p>Search and find all information about it.</p>
             </div>
-            <div className="floating-ball">
-              <Pokeball />
-            </div>
             <PokeballConcept />
           </div>
-          <div className="divider">
-            <ShapeDividerTriangle />
-          </div>
         </section>
+        <div className="floating-ball">
+          <Pokeball />
+        </div>
         <section>
           <h2>Pokedex</h2>
           <Pokedex />
